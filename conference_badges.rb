0909b4a70275fd8398e714b6ffbc34d02ebfed84
Patrick J Louis <lumenfuzz@gmail.com>
2018-03-11 17:03:00 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,12 +4,20 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees.each do |attendent|
-    "Hello, my name is #{attendent}"
+  attendees.each do |attendant|
+    "Hello, my name is #{attendant}"
   end
 end
 
 def assign_rooms(attendees)
+  counter = 0
+  attendees.each do |attendant|
+    "Hello, #{attendant}! You'll be assigned to room #{counter}!"
+    counter += 1
+  end
+end
+
+def printer
 end
 
 def badges_and_room_assignments
